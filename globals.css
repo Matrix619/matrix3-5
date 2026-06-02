7fac5a3fe33bca926b657794ddf747d4eabbf360
Matrix619 <278044903+Matrix619@users.noreply.github.com>
2026-06-01 20:51:07 -0500

    Add files via upload

---
+++ globals.css
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Matrix Mech Co",
+  description: "Residential, commercial, and industrial HVAC and mechanical services."
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
